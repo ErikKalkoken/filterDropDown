dddb408298130a227f081d37d7a4d3e5d43259f3
Erik Kalkoken <erik.kalkoken@gmail.com>
2017-08-28 15:48:21 +0200

    updates to exampels

--- js/filterDropDown.js
+++ js/filterDropDown.js
@@ -3,30 +3,8 @@
  *
  * Copyright (C) 2017 Erik Kalkoken
  *
- * Extension for DataTables v1.10.0 (developed and test with v1.10.15)
+ * Extension for DataTables v1.10.0 (developed and tested with v1.10.15)
  *
- * Adds a dropDown element for selected columns to a Datatable
- * allowing the user to filter the table to only show rows containing
- * a certain value. e.g. in a list of employees to only show the ones 
- * that have an office in a certain city.
- *
- * Features:
- * - Optionally works with Bootstrap 3 styling
- * - Uses column header as default title, can be overriden with custom title
- * - CSS styles and/or CSS classes can be applied to each filter element
- *
- * Usage:
- * 1. Include this file into your html file your after the inclusion of dataTables JS
- * 2. Add a filterDropDown section in the DataTables initialisation object
- * 
- * Example:
- *	$('#example').DataTable({
- * 		filterDropDown: {									
- *			columns: [
-				idx: 3
-			]
- *		}
- *	} );
  *
 **/
 
